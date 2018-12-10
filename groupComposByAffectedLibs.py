1c1ad11dc9f055ef48583d3355d15a71bb96afec
RajNalluri <raj09476@gmail.com>
2018-12-10 15:01:17 +0530

    Uploaded python script
    The python script takes input csv file in the format <Component,Impacting Library> (e.g., <a,1;a,2;b,1;c,2;c,4;>) and generates csv file in the format <Component,list of impacting libraries>(i.e., <a,[1,2];b,1,c,[2,4]>)

---
+++ groupComposByAffectedLibs.py
@@ -0,0 +1,19 @@
+from pandas import read_csv
+import json
+
+df = read_csv('affectedComponents.csv')
+impactjson = {}
+for index, row in df.iterrows():
+    impactjson[str(row["NW Component"])] = []
+for index, row in df.iterrows():
+    impactjson[str(row["NW Component"])].append(str(row["Impacting library"]))
+print(impactjson)
+
+o = open("impactjson.json","w")
+o.write(json.dumps(impactjson))
+o.close()
+
+i = open("outputfile.csv","w")
+i.write("NW Component, Impacting Library\n")
+for each in impactjson:
+    i.write(each+","+"\""+str(impactjson[each]).strip("[").strip("]")+"\""+"\n")
